4a49249f7d86b7dd92a366e880939b327027066d
Vietprole <phuvietdn@gmail.com>
2025-03-02 11:00:49 +0700

    Fix write permission denied

--- api/server.js
+++ api/server.js
@@ -1,7 +1,16 @@
 const jsonServer = require('json-server');
 const jsonServerAuth = require('json-server-auth');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+
+// Uncomment to allow write operations
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
+
+// const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
