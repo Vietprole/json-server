50e322557dbb635ab59db66113886b7e43de41af
Vietprole <phuvietdn@gmail.com>
2025-03-03 14:23:35 +0700

    Locally, don't use the write operations

--- api/server.js
+++ api/server.js
@@ -3,14 +3,14 @@ const jsonServerAuth = require('json-server-auth');
 const server = jsonServer.create();
 
 // Uncomment to allow write operations
-const fs = require('fs')
-const path = require('path')
-const filePath = path.join('db.json')
-const data = fs.readFileSync(filePath, "utf-8");
-const db = JSON.parse(data);
-const router = jsonServer.router(db)
+// const fs = require('fs')
+// const path = require('path')
+// const filePath = path.join('db.json')
+// const data = fs.readFileSync(filePath, "utf-8");
+// const db = JSON.parse(data);
+// const router = jsonServer.router(db)
 
-// const router = jsonServer.router('db.json');
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
